ef95e4908a306440b9a9de4fbec1c21b885f979a
Grimreaper <danielbeck22@gmail.com>
2026-05-10 21:01:57 -0600

    Worked on mining work orders think i have it finished.

--- HellsingLogistics.Domain/Mining Work Orders/MiningWorkOrder.cs
+++ HellsingLogistics.Domain/Mining Work Orders/MiningWorkOrder.cs
@@ -1,26 +1,25 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using HellsingLogistics.Domain.Common;
 
 namespace HellsingLogistics.Domain.MiningWorkOrders;
 
-public class MiningWorkOrder
+public class MiningWorkOrder : BaseEntity
 {
-    public int Id { get; set; }
-    public string WorkOrderNumber { get; set; } = string.Empty;
+    public MiningWorkOrder ()
+    {
+        
+        WorkOrderNumber = $"MWO-{DateTime.Now.ToString("yyyyMMddHHmmss")}";
 
-    public int ProductionRequestId { get; set; }
-    public ProductionRequest ProductionRequest { get; set; } = null!;
+    }
 
+    public String WorkOrderNumber { get; set; }
+    
     public WorkOrderStatus Status { get; set; } = WorkOrderStatus.Open;
+    
     public PriorityLevel Priority { get; set; } = PriorityLevel.Normal;
-
-    public int? AssignedToUserId { get; set; }
-    public AppUser? AssignedToUser { get; set; }
-
-    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+ 
     public DateTime? CompletedAtUtc { get; set; }
-
-    public string? Notes { get; set; }
-
+    
     public ICollection<MiningWorkOrderMaterial> MaterialsNeeded { get; set; } = new List<MiningWorkOrderMaterial>();
 }
 
